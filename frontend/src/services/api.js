87b709145509a53d245146b6d2fcdf1e4ea621bf
Mijanur Rahman Oli <sheikhmijanurrahmanoli@gmail.com>
2026-01-22 18:03:13 +0600

    live site

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL 
+  ? `${process.env.REACT_APP_API_URL}/api` 
+  : 'http://localhost:5000/api';
 
 // Create axios instance
 const api = axios.create({
